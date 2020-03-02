ffb8644385cc28feede55e633e184dad5512571e
JTLiner925 <[jtliner925@gmail.com]>
2020-03-02 13:37:45 -0600

    display number of dogs

--- index.js
+++ index.js
@@ -1,36 +1,35 @@
 'use strict';
 
-function getDogImage() {
-    
-  fetch('https://dog.ceo/api/breeds/image/random')
+function getDogImage(num) {  
+  fetch(`https://dog.ceo/api/breeds/image/random/${num}`)
     .then(response => response.json())
     .then(responseJson => 
       displayResults(responseJson))
     .catch(error => alert('Something went wrong. Try again later.'));
 }
 
-function displayResults(responseJson, num) {
+function displayResults(responseJson) {
   console.log(responseJson);
-  const pics = $("#number").val();
-  //replace the existing image with the new one
-  $('.results-img').replaceWith(
-    `<img src="${responseJson.message}" class="results-img">`
-  )
+
+  // .replaceWith(
+  //   `<img src="${responseJson.message}" class="results-img">`);  
+  responseJson.message.map(img => $('.results-img').append(`<li class='image'><img src=${img}></li>`));
+
   //display the results section
   $('.results').removeClass('hidden');
 }
-// function numOfPics(){
-//     $(document).on('input', '#number', function(){
-//       var pics = $("#number").val();
-//       $("#reward").text(pics);
-//   })
-//   }
+
 function watchForm() {
   $('form').submit(event => {
     event.preventDefault();
-    
-    getDogImage();
-  });
+    const pics = $("#number").val();
+    console.log(pics);
+    if(pics > 50){
+      alert('You can only view 50 or less at a time');
+    }else{
+      getDogImage(pics);
+  }
+});
 }
 
 $(function() {
