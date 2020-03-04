790774bc54f7c10f86351bc38ca93702c9721c12
JTLiner925 <[jtliner925@gmail.com]>
2020-03-04 08:24:06 -0600

    match input to breed using includes() method

--- breed.js
+++ breed.js
@@ -1,27 +1,19 @@
 'use strict';
-function getBreedList(){
-  fetch('https://dog.ceo/api/breeds/list/all')
-    .then(response => response.json())
-    .then(responseJson => 
-      console.log(responseJson))
-    .catch(error => alert('Please check your spelling and try again.'));
-}
-
 function getDogImage(breed) {
     
-  fetch(`https://dog.ceo/api/breeds/${breed}/images`)
-    .then(res => res.json())
-    .then(resJson => 
-      displayResults(resJson))
+  fetch(`https://dog.ceo/api/breed/breed_name/sub_breed/images/random`)
+    .then(response => response.json())
+    .then(responseJson => 
+      displayResults(responseJson))
     .catch(error => alert('Something went wrong. Try again later.'));
 }
 
-function displayResults(resJson) {
-  console.log(resJson);
+function displayResults(responseJson) {
+  console.log(responseJson);
   //replace the existing image with the new one
   $('.results-img').replaceWith(
-    `<img src="${resJson.message}" class="results-img">`
-  )
+    `<img src="${responseJson.message}" class="results-img">`
+  );
   //display the results section
   $('.results').removeClass('hidden');
 }
@@ -29,22 +21,20 @@ function displayResults(resJson) {
 function watchForm() {
   $('form').submit(event => {
     event.preventDefault();
-     
-    // if(dogBreed !== ){
-    //     alert('')
-    const dogBreed = $("#breed").val(); 
-    const dogBreedList = getBreedList();
+    const dogBreed = $("#breed").val().toLowerCase(); 
     console.log(dogBreed);
-   
+    const breedChose = `${responseJson.message}.map(dog => ${dog}.includes(${dogBreed}))`;
+    
+   if(dogBreed === breedChose ){
+    getDogImage(dogBreed);
+   } else {
+       alert('Check your spelling, try again')
+   }
     // dogBreedList.map(breed !== dogBreed)
-    for(let i = 0; i< dogBreedList; i++){
-      const listOfBreeds = dogBreedList[i];
-      if(dogBreed !== listOfBreeds){
-        alert('Oops! maybe it was misspelled...');
-      } else {
-        getDogImage(dogBreed);
-      }
-    }
+    
+    
+      
+    
   });
 }
 
